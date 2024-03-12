e19058c4f220d7d982f879f3f18c9725b7088ef9
Daniel Fabián <daniel@conectaentradas.com>
2024-03-12 15:58:57 +0100

    Add html page

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory = './templates')
 
 
-@app.get("/")
-def root():
-  # Empty commit
-  return {"message": "Hello World"}
+@app.get("/", response_class = HTMLResponse)
+def root(request: Request):
+  return templates.TemplateResponse(
+    request = request, name = 'index.html', context = {}
+  )
