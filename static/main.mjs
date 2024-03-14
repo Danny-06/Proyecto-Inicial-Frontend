3b10e7638cf32f76a5c712e01b485a95f7fdcb0a
Daniel Fabián <daniel@conectaentradas.com>
2024-03-14 10:46:33 +0100

    Update main.mjs

--- static/main.mjs
+++ static/main.mjs
@@ -1,7 +1,11 @@
+const API_ORIGIN = 'https://'
+
 async function sendData(data) {
+  const endpoint = new URL('/api/send-data', API_ORIGIN)
+
   const stringifiedData = JSON.stringify(data)
 
-  const response = await fetch('/api/send-data', {
+  const response = await fetch(endpoint, {
     method: 'POST',
     body: stringifiedData,
     headers: {
